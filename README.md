ad98c22656212d034bdbdd55009a8b4c09e761b2
Abigail Limpioso <alimpioso@gmail.com>
2021-01-18 16:13:53 -0500

    working on responsive images. Added text to show what search results were for. fixed some bugs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Image Search
 
+This application utilizes the Flickr API to search for photos and display the first 20 results based on the keywords the user enters.
--- public/js/script.js
+++ public/js/script.js
@@ -14,13 +14,11 @@ function searchImg() {
     //get value of search input
     var searchInput = $("#search-img").val();
 
-    console.log(searchInput);
+    //console.log(searchInput);
 
     if(searchInput.length > 0) {
-        //clears response if any
-        $('#search-response').text("");
         //clears results if any
-        $('#pic-results').text('');
+        $('#pic-results').html(`<p id="search-response">Search Results for "${searchInput}":</p>`);
 
         let url = `https://api.flickr.com/services/rest?api_key=165ca9e8101be3f19d41beeb4bc2e8c2&format=json&method=flickr.photos.search&safe_search=1&nojsoncallback=1&text=${searchInput}&tags=${searchInput}`;
 
@@ -30,15 +28,18 @@ function searchImg() {
 
             if(data != '') {
 
+                //getting only the first 20 results
                 let firstSet = data['photos']['photo'].slice(0,20);
                 //console.log('20 results - '+JSON.stringify(firstSet));
                 
                 let resultsHtml = '';
 
+                //adding images to results div
                 firstSet.map((img) => {
                     resultsHtml += `<img src="https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}_q.jpg" alt="${img.title}"/>`;
                 });
 
+                //appending images to div
                 $("#pic-results").append(resultsHtml);
 
             }else {
