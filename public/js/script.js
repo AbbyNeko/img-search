adc1f34fb23dec62136d0b1e2587a43881ad3672
Abigail Limpioso <alimpioso@gmail.com>
2021-01-18 00:46:28 -0500

    added changes for flickr request

--- public/js/script.js
+++ public/js/script.js
@@ -12,6 +12,21 @@ function searchImg() {
         //clears response if any
         $('#search-response').text("");
 
+        let url = 'https://api.flickr.com/services/rest?api_key=165ca9e8101be3f19d41beeb4bc2e8c2&method=flickr.photos.search&safe_search=1&text='+searchInput;
+
+        $.get(url, (data) => {
+
+            console.log('results - '+JSON.stringify(data));
+
+            if(data.length > 0) {
+
+
+            }else {
+                $('#search-response').text("No Results found :(");
+            }
+
+        });
+
     } else {
         $('#search-response').text("Please enter something :(");
     }
