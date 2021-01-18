1c34827d7d5f7ef1813259e9843360ed56a6a07e
Abigail Limpioso <alimpioso@gmail.com>
2021-01-18 02:04:42 -0500

    got search working and images

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,12 @@
+
+//button event listener
 document.getElementById('search-btn').addEventListener('click', searchImg);
+
+//event listener if enter is pressed
+$('form').submit((evt) => {
+    evt.preventDefault();
+    searchImg();
+});
 
 //does a Flickr image request and shows first 20 images
 function searchImg() {
@@ -11,15 +19,27 @@ function searchImg() {
     if(searchInput.length > 0) {
         //clears response if any
         $('#search-response').text("");
+        //clears results if any
+        $('#pic-results').text('');
 
-        let url = 'https://api.flickr.com/services/rest?api_key=165ca9e8101be3f19d41beeb4bc2e8c2&method=flickr.photos.search&safe_search=1&text='+searchInput;
+        let url = 'https://api.flickr.com/services/rest?api_key=165ca9e8101be3f19d41beeb4bc2e8c2&format=json&method=flickr.photos.search&safe_search=1&nojsoncallback=1&text='+searchInput;
 
-        $.get(url, (data) => {
+        $.get(url, function(data) {
 
-            console.log('results - '+JSON.stringify(data));
+            //console.log('results - '+JSON.stringify(data));
 
-            if(data.length > 0) {
+            if(data != '') {
 
+                let firstSet = data['photos']['photo'].slice(0,20);
+                //console.log('20 results - '+JSON.stringify(firstSet));
+                
+                let resultsHtml = '';
+
+                firstSet.map((img) => {
+                    resultsHtml += `<img src="https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}_q.jpg" alt="${img.title}"/>`;
+                });
+
+                $("#pic-results").append(resultsHtml);
 
             }else {
                 $('#search-response').text("No Results found :(");
